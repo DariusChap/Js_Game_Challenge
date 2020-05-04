1b66c7a0fb4c3f3ab911406fc689736dbfd71a6f
Darius <chapd289@robertmorris.edu>
2020-05-04 16:23:26 -0500

    game finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,75 @@
+let count = 0;
+let count1 = 0;
+let goals = 0;
+let resetCount = 0;
+const teamoneButton = document.querySelector("#teamone-shoot-button");
+const teamoneNumShots = document.querySelector("#teamone-numshots");
+const teamtwoButton = document.querySelector("#teamtwo-shoot-button");
+const teamtwoNumShots = document.querySelector("#teamtwo-numshots");
+const teamoneNumGoals = document.querySelector("#teamone-numgoals");
+const teamtwoNumGoals = document.querySelector("#teamtwo-numgoals");
+const resetButton = document.querySelector("#reset-button");
+const resetNumber = document.querySelector("#num-resets");
+
+
+teamoneButton.addEventListener("click", function() {
+    count = parseInt(teamoneNumShots.innerText) + 1;
+    teamoneNumShots.innerText = count;
+    
+    
+    const rand = Math.random();
+    if (rand > 0.5) {
+        console.log("Greater than" + rand)
+        goals = parseInt(teamoneNumGoals.innerText) + 1;
+        teamoneNumGoals.innerText = goals;
+    } else {
+        console.log("Less than" + rand)
+    }
+
+
+})
+
+
+teamtwoButton.addEventListener("click", function() {
+    count1 = parseInt(teamtwoNumShots.innerText) + 1;
+    teamtwoNumShots.innerText = count1;
+
+    const rand = Math.random();
+    if (rand > 0.5) {
+        console.log("Greater than" + rand)
+        goals = parseInt(teamtwoNumGoals.innerText) + 1;
+        teamtwoNumGoals.innerText = goals;
+    } else {
+        console.log("Less than" + rand)
+    }
+    
+
+})
+
+resetButton.addEventListener("click", function() {
+    resetCount = parseInt(resetNumber.innerText) + 1;
+    resetNumber.innerText = resetCount;
+    teamoneNumShots.innerText = 0;
+    teamtwoNumShots.innerText = 0;
+    teamoneNumGoals.innerText = 0;
+    teamtwoNumGoals.innerText = 0;
+
+})    
 
 
 
 
+
+
+
+
+
+
+
+
+
+   
+    
+
+
+ 
