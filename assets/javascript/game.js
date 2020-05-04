4cdac9dc40748f63a3f734ab5da9f405ad082c9a
Darius <chapd289@robertmorris.edu>
2020-05-04 18:23:54 -0500

    game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,8 @@ const resetNumber = document.querySelector("#num-resets");
 teamoneButton.addEventListener("click", function() {
     count = parseInt(teamoneNumShots.innerText) + 1;
     teamoneNumShots.innerText = count;
-    var swoosh = new Audio('')
+    let swoosh = new Audio('assets/images/glitch.mp3')
+    swoosh.play();
 
 
     const rand = Math.random();
@@ -23,8 +24,12 @@ teamoneButton.addEventListener("click", function() {
         console.log("Greater than" + rand)
         goals = parseInt(teamoneNumGoals.innerText) + 1;
         teamoneNumGoals.innerText = goals;
+        let swoosh = new Audio('assets/images/Arrow+Swoosh+1.mp3')
+    swoosh.play();
     } else {
         console.log("Less than" + rand)
+        let swoosh = new Audio('assets/images/bow.mp3')
+    swoosh.play();
     }
 
 
@@ -34,14 +39,20 @@ teamoneButton.addEventListener("click", function() {
 teamtwoButton.addEventListener("click", function() {
     count1 = parseInt(teamtwoNumShots.innerText) + 1;
     teamtwoNumShots.innerText = count1;
+    let swoosh = new Audio('assets/images/glitch.mp3')
+    swoosh.play();
 
     const rand = Math.random();
     if (rand > 0.5) {
         console.log("Greater than" + rand)
         goals = parseInt(teamtwoNumGoals.innerText) + 1;
         teamtwoNumGoals.innerText = goals;
+        let swoosh = new Audio('assets/images/Arrow+Swoosh+1.mp3')
+    swoosh.play();
     } else {
         console.log("Less than" + rand)
+        let swoosh = new Audio('assets/images/bow.mp3')
+    swoosh.play();
     }
     
 
@@ -50,6 +61,8 @@ teamtwoButton.addEventListener("click", function() {
 resetButton.addEventListener("click", function() {
     resetCount = parseInt(resetNumber.innerText) + 1;
     resetNumber.innerText = resetCount;
+    let swoosh = new Audio('assets/images/01-power-up-mario.mp3')
+    swoosh.play();
     teamoneNumShots.innerText = 0;
     teamtwoNumShots.innerText = 0;
     teamoneNumGoals.innerText = 0;
